7b67220614b86ad6d466d41a752385a44360a499
Natasha Wilson <wilson.natasharose@gmail.com>
2020-05-02 14:55:45 +0100

    add client class

---
+++ udp_client.py
@@ -0,0 +1,21 @@
+import logging, socket
+import constants
+
+class UdpClient():
+    def __init__(self):
+        self.logger = logging.getLogger()
+
+    def run(self):
+        s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+        while True:
+            message = input('Enter your date of birth to find out what day of the week it was\nType dob as dd-mm-yyyy here: ')
+            data = message.encode(encoding='ascii')
+            s.sendto(data, ('127.0.0.1', 3000))
+            self.logger.info('The OS assigned the address {} to client'.format(s.getsockname()))
+            data, address = s.recvfrom(constants.MAX_SIZE_BYTES)
+            text = data.decode(encoding='ascii')
+            print('The server {} replied with {!r}'.format(address, text))
+
+if __name__ == '__main__':
+    UdpClient().run()
